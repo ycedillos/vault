b27af7b37f287bcb592d36ec745a28cd02c26179
yonathan-cedillos <yonathan.cedillos@dumboldspider.com>
2022-01-23 11:33:03 -0600

    show versions

--- ci-cd/scripts/deployment.sh
+++ ci-cd/scripts/deployment.sh
@@ -10,4 +10,11 @@ install_vault
 # Login into GKE
 gcp_login
 
+echo " ******** gcloud version ******* "
 gcloud --version
+
+echo " ******** helm version ******* "
+helm version
+
+echo " ******** vault version ******* "
+vault -v
