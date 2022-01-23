b18e674ae9230710442bf01d9354552c326108a7
yonathan-cedillos <yonathan.cedillos@dumboldspider.com>
2022-01-23 11:53:16 -0600

    pruebas

--- ci-cd/scripts/deployment.sh
+++ ci-cd/scripts/deployment.sh
@@ -18,3 +18,11 @@ helm version
 
 echo " ******** vault version ******* "
 vault -v
+
+echo "Show environment variables"
+
+echo ${CIRCLE_REPOSITORY_URL}
+echo ${CIRCLE_SHA1}
+echo ${CIRCLE_PR_NUMBER}
+echo ${CIRCLE_PR_REPONAME}
+echo ${CIRCLE_PROJECT_REPONAME}
